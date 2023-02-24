9413d49dd0ed6a7340e514e8b0819bc082ae2e04
LuisViera99 <luis.viera@cceo.com.mx>
2023-02-24 14:44:05 -0600

    Ordenamiento de Paises

---
+++ src/components/CardList.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="row">
+        <div class="col-12">
+            {{ paises }}
+        </div>
+    </div>
+</template>
+
+<script>
+import { onMounted, computed } from 'vue';
+import {useStore} from 'vuex'
+export default {
+    setup(){
+        const store = useStore()
+        const paises =  computed (() =>{
+            return store.getters.topPaisesPoblacion
+
+        })
+    
+        onMounted(() =>{
+           store.dispatch('getPaises')
+          
+
+        })
+
+
+        return {paises}
+    }
+
+
+}
+
+
+</script>
--- src/store/index.js
+++ src/store/index.js
@@ -2,12 +2,40 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    paises:[],
+    paisesFiltrados: []
   },
   getters: {
   },
   mutations: {
+    setPaises(state, payload){
+      state.paises = payload
+    },
+    setpaisesFiltrados(state, payload){
+      state.paisesFiltrados = payload
+    }
+
   },
   actions: {
+    async getPaises({commit}){
+        try {
+          const res = await fetch('https://restcountries.com/v3/all')
+          const data = await res.json()
+          //console.log(data)
+          commit('setPaises', data)
+          
+        } catch (error) {
+          console.log(error)
+        }
+    }
+  },
+
+  getters:{
+    topPaisesPoblacion(state){
+      return state.paises.sort((a,b) => {
+        return a.population < b.population ? 1: -1
+      })
+    }
   },
   modules: {
   }
